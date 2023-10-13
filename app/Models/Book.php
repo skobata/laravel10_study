733e8379299f53575a67a56ddb333d77eeb3ffb9
skobata <pf.s.kobata@gmail.com>
2023-10-13 19:10:12 +0900

    データベースとモデル途中まで

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
 {
     use HasFactory;
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 
     public function authors(): BelongsToMany
     {
